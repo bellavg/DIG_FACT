b3c290d53a375d39f8f0ed19e9e77413e78100e8
bellavg <isabella.v.gardner@gmail.com>
2024-01-21 16:21:52 +0100

    done with hpo

--- docs/ir.sh
+++ docs/ir.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
 #SBATCH --job-name=digin
-#SBATCH --time=00:45:00
+#SBATCH --time=02:45:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
 #SBATCH --output=slurm_output_%A.out
@@ -13,8 +13,16 @@ module load CUDA/11.8.0
 module load Anaconda3/2022.05
 
 
-conda env create -f ./docs/environment.yaml
+conda env update  --file environment.yaml
 
 # Activate your environment
-source activate fact_env
-pip install .
+source activate dig
+#cd DIG
+#pip install 
+#pip install pyro-ppl
+#pip install torch_geometric
+#pip install torch_scatter 
+#pip install torch_sparse 
+#pip install torch_cluster 
+#pip install torch_spline-conv
+
